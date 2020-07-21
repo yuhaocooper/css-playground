c7fb50f64ac2513b507c37f8cd826eb9734c0504
yuhao tan <tanyuhao@hotmail.com>
2020-07-21 12:56:37 +0800

    remove cache of key

--- client/src/Webapp.js
+++ client/src/Webapp.js
@@ -3,9 +3,6 @@ import React, {useEffect} from 'react'
 function Webapp(){
 
   useEffect(() => {
-
-    var apikey = 'AIzaSyCEB_8OvOMItGZvgyKSX9NGFX_gsMtb9J4'
-
     fetch('https://ghibliapi.herokuapp.com/films')
         .then(response => response.json())
         .then(data => console.log(data))
